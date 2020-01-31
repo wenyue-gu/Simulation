cc72f3616b905537bfd6d27cd1d981e41f284676
Michelle Tai <michelle.tai@duke.edu>
2020-01-30 23:13:44 -0500

    updated percolation simulation

--- src/cellsociety/Percolation.java
+++ src/cellsociety/Percolation.java
@@ -1,64 +1,74 @@
 package cellsociety;
 
-import java.util.ArrayList;
-
+import java.util.List;
+/**
+* Percolation class holds the rules and update behavior for the percolation simulation
+*
+* @author Michelle Tai
+**/
 public class Percolation extends Simulation {
   private int OPEN = 0;
   private int CLOSED = 1;
   private int PERCOLATED = 2;
-  private boolean hasChanged;
-  public Percolation(ArrayList<ArrayList<Cell>> grid){
+
+  /**
+   * Constructor for the Percolation class that sets its grid to the grid that is passed in as an argument
+   * @param grid is a 2d list that holds all the cells that will appear in the simulation
+   * @author Michelle Tai
+   * */
+  public Percolation(List<List<Cell>> grid){
     super(grid);
   }
 
+  /**
+   * Updates the cells in the Percolation simulation's grid to the next state
+   * Also updated the look of the cell
+   * @author Michelle Tai
+   * */
   @Override
   public void updateGrid(){
-
-    for(ArrayList<Cell> rows: getCellGrid()){
+    for(List<Cell> rows: cellGrid){
       for(Cell cell:rows){
-        checkNeighbourAndChangeNext(cell, cell.findNeighbours(getCellGrid(), 8));
+        checkNeighbourAndChangeNext(cell, cell.findNeighbours(cellGrid, 8));
       }
     }
-    for(ArrayList<Cell> rows: getCellGrid()){
+    for(List<Cell> rows: cellGrid){
       for(Cell cell:rows){
         cell.updateColor();
       }
     }
   }
 
+  /**
+   * Updates the cell's state to percolated if any of its neighbors is percolated & the cell itself is open
+   * @param cell the cell that is currently being checked
+   * @param neighbours are the neighbors of the cell
+   */
   @Override
-  public void checkNeighbourAndChangeNext(Cell cell, ArrayList<Cell> neighbours){
+  public void checkNeighbourAndChangeNext(Cell cell, List<Cell> neighbours){
     for(Cell neighbour:neighbours){
       if(cell.getCurrentState() == OPEN && neighbour.getCurrentState()== PERCOLATED) {
         cell.changeNext(PERCOLATED);
-//        hasChanged = true;
       }
     }
   }
 
+  /**
+   * Returns whether or not the simulation has "percolated," aka there a path of percolated blue cells
+   * from the left side of the grid to the right side of the grid
+   * @return a boolean that indicates whether or not the simuation has percolated
+   * @author Michelle Tai
+   */
   public boolean hasPercolated(){
-    return leftRightHasPercolated(this.getCellGrid()) && topBotHasPercolated(this.getCellGrid());
-  }
-
-  public boolean didNotChange(){
-    boolean didNotUpdate = false;
-    for(ArrayList<Cell> rows: getCellGrid()){
-      for(Cell cell:rows){
-        if(cell.getCurrentState() != cell.ne)
-      }
-    }
-  }
-
-  private boolean leftRightHasPercolated(ArrayList<ArrayList<Cell>> grid){
-    int numOfRows = grid.size();
-    int numOfCols = grid.get(0).size();
+    int numOfRows = cellGrid.size();
+    int numOfCols = cellGrid.get(0).size();
     boolean hasFirstPerc = false;
     boolean hasLastPerc = false;
     Cell leftCell, rightCell;
 
     for(int i = 0; i < numOfRows; i++){
-      leftCell = grid.get(i).get(0);
-      rightCell = grid.get(i).get(numOfCols - 1);
+      leftCell = cellGrid.get(i).get(0);
+      rightCell = cellGrid.get(i).get(numOfCols - 1);
       if(leftCell.getCurrentState() == PERCOLATED){
         hasFirstPerc = true;
       }
@@ -69,23 +79,20 @@ public class Percolation extends Simulation {
     return hasFirstPerc && hasLastPerc;
   }
 
-  private boolean topBotHasPercolated(ArrayList<ArrayList<Cell>> grid){
-    int numOfRows = grid.size();
-    int numOfCols = grid.get(0).size();
-    boolean hasFirstPerc = false;
-    boolean hasLastPerc = false;
-    Cell topCell, botCell;
-
-    for(int i = 0; i < numOfCols; i++){
-      topCell = grid.get(0).get(i);
-      botCell = grid.get(numOfRows - 1).get(i);
-      if(topCell.getCurrentState() == PERCOLATED){
-        hasFirstPerc = true;
-      }
-      if(botCell.getCurrentState() == PERCOLATED){
-        hasLastPerc = true;
+  /**
+   * Checks whether or not the cells did change. If the cells did not change, all the currentStates are
+   * equal to their next states
+   * @return a boolean, true is did not change, false if did change
+   */
+  public boolean didNotChange(){
+    for(List<Cell> rows: cellGrid){
+      for(Cell cell:rows){
+        if(cell.getCurrentState() != cell.getNextState()){
+          return false;
+        }
       }
     }
-    return hasFirstPerc && hasLastPerc;
+    return true;
   }
+
 }
