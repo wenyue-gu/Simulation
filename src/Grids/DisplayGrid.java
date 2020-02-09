3273ad3197c47a2584a64ee9499ca1ec14becf1f
wenyue gu <wg74@duke.edu>
2020-02-09 18:08:28 -0500

    xml file has shape tag

---
+++ src/Grids/DisplayGrid.java
@@ -0,0 +1,39 @@
+package Grids;
+
+import Cells.RectCell;
+import View.SimulationViewGUI;
+import cellsociety.Cell;
+import cellsociety.Grid;
+import java.util.*;
+
+public class RectGrid extends Grid {
+
+    public RectGrid(int row, int col, boolean allNeighbour, boolean wrap){
+        super(wrap);
+        if(allNeighbour) numNeighbour = 8;
+        else numNeighbour = 4;
+        grid = new ArrayList<>();
+        for(int i = 0; i<row; i++){
+            grid.add(new ArrayList<>());
+            for(int j=0; j<col; j++){
+                Cell cell = new RectCell(i, j, (double)SimulationViewGUI.SUBSCENE_WIDTH/col,
+                        (double)SimulationViewGUI.SUBSCENE_HEIGHT/row, 0);
+                grid.get(i).add(cell);
+            }
+        }
+    }
+
+
+
+    @Override
+    public int[] rowH(int[]index) {
+        return new int[]{-1, 1, 0, 0, -1, 1, -1, 1};
+    }
+
+    @Override
+    public int[] colH(int[]index) {
+        return new int[]{0, 0, -1, 1, 1, -1, -1, 1};
+    }
+
+
+}
